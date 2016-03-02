efea70e775ab860208f61502a44ac55662e8c59e
Thomas Roberts <tom.p.roberts@gmail.com>
2016-03-02 19:06:49 +0000

    Added structured way of storing imaged

--- birdWater.py
+++ birdWater.py
@@ -18,20 +18,31 @@ import RPi.GPIO as GPIO
 # Define for log file
 logFile="/var/log/birdWatcher.log"
 
+# Define the base path
+basePath="/home/pi/birdWatcherImages"
+
 # GPIO for PIR
 PIR = 4
 
-
-
-def getFileName():
-	return datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
+def getPath():
+	# Build the path for directory
+	date = datetime.datetime.now().strftime("%d-%m-%Y")
+	path = os.path.abspath(basePath + "/" + date)
+	# Check is the path exists
+	if not os.path.isdir(path):
+		logging.debug("Making %s directory", path)
+		os.makedirs(path)
+	return path
+	
 
 
 def takePhoto():
-	for i in range(1,2):
-		capturename = getFileName()
-		logging.info("Motion detected! Taking snapshot %s", capturename)
-		cmd="raspistill -w 640 -h 480 -n -t 10 -q 10 -e jpg -th none -o /home/pi/" + capturename+"_%d.jpg" % (i)
+	for i in range(1,6):
+		time = datetime.datetime.now().strftime("%H.%M.%S")
+		capturePath = os.path.abspath(getPath() + "/" + time + "_%d.jgp" % (i))
+		logging.info("Motion detected! Taking snapshot %s_%d.jpg", capturePath, i)
+		#cmd="raspistill -w 640 -h 480 -n -t 10 -q 10 -e jpg -th none -o " + capturePath
+		cmd="touch " + capturePath
 		camerapid = subprocess.call(cmd,shell=True)
 
 if __name__ == "__main__":
